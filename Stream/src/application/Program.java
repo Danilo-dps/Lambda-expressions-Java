7e71ece229f6d85df7911583f8ea0f5ff7dd64c2
Danilo Pereira da silva <danilo@marighella.danilo>
2025-01-06 23:04:40 -0300

    A interface funcional Stream

---
+++ Stream/src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/* Artigo bacana sobre streams
+		 * http://www.oracle.com/technetwork/pt/articles/java/streams-api-java-8-3410098-ptb.html
+		 */
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		
+		System.out.println();
+		System.out.println("A coleção declarada é: ");
+		System.out.println(Arrays.toString(list.toArray()));
+		System.out.println();
+		
+		System.out.println("Criando uma stream a partir de uma coleção");
+		Stream<Integer> stA = list.stream();
+		System.out.println(Arrays.toString(stA.toArray()));
+		System.out.println();
+		
+		System.out.println("Criando uma stream a partir de uma coleção, multiplicando cada item por 10");
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+		System.out.println();
+		
+		System.out.println("Criando uma stream usando o método Stream.of:");
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		System.out.println();
+
+		System.out.println("Criando uma stream usando o método Stream.iterate:");
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		System.out.println();
+		
+		System.out.println("Criando uma stream usando o método Stream.iterate da sequência fibonacci:");
+		Stream<Long> st4 = Stream.iterate(new long[]{ 0L, 1L }, p->new long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
+		System.out.println(Arrays.toString(st4.limit(25).toArray()));
+	}
+}
